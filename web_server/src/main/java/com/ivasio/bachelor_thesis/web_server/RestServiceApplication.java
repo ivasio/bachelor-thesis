2900de322e96be9a0cf112dba431505f37482953
Ivan Chebotar <van.chebotar@yandex.ru>
2020-03-21 23:34:54 +0300

    new: set up PostgreSQL usage in web server

--- web_server/src/main/java/com/ivasio/bachelor_thesis/web_server/RestServiceApplication.java
+++ web_server/src/main/java/com/ivasio/bachelor_thesis/web_server/RestServiceApplication.java
@@ -2,7 +2,10 @@ package com.ivasio.bachelor_thesis.web_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import org.springframework.context.annotation.Bean;
+
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -11,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.ivasio.bachelor_thesis")
 @EnableSwagger2
 public class RestServiceApplication {
 
--- web_server/src/main/java/com/ivasio/bachelor_thesis/web_server/controllers/JunctionController.java
+++ web_server/src/main/java/com/ivasio/bachelor_thesis/web_server/controllers/JunctionController.java
@@ -1,12 +1,8 @@
-package com.ivasio.bachelor_thesis.web_server;
+package com.ivasio.bachelor_thesis.web_server.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,40 +10,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.ivasio.bachelor_thesis.shared.Junction;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
+import com.ivasio.bachelor_thesis.shared.models.Junction;
+import com.ivasio.bachelor_thesis.web_server.services.JunctionService;
 
 
 @RestController
 @RequestMapping("/junctions")
 public class JunctionController {
 
-    private final ConcurrentHashMap<Integer, Junction> allJunctions;
+    @Autowired
+    private JunctionService service;
 
+    /*
     public JunctionController() {
         allJunctions = new ConcurrentHashMap<Integer, Junction>();
         allJunctions.put(1, new Junction(1, "МКАД - ш. Энтузиастов", 37.84270f, 55.77692f));
         allJunctions.put(2, new Junction(2, "МКАД - Каширское ш.", 37.72944f, 55.59180f));
         allJunctions.put(3, new Junction(3, "МКАД - Рязанский пр.", 37.83499f, 55.70789f));
     }
+    */
 
     @GetMapping("/")
     @ApiOperation(value = "Получить список всех известных дорожных развязок", response = Junction.class)
     public List<Junction> listAllJunctions() {
-        return new ArrayList<Junction>(allJunctions.values());
+        return service.listAll();
     }
 
     @GetMapping("/{id}")
     @ApiOperation(value = "Получить информацию о дорожной развязке по ее id", response = Junction.class)
     public Junction getJunctionById(
-            @ApiParam(value = "id дорожной развязки", required = true, example = "1") @PathVariable("id") int id
+            @ApiParam(value = "id дорожной развязки", required = true, example = "1") @PathVariable("id") Long id
     ) {
-        Junction result =  allJunctions.get(id);
-        if (result != null) {
-            return result;
-        } else {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Junction with id %d not found", id));
-        }
+        return service.get(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        String.format("Junction with id %d not found", id))
+        );
     }
 
 }
