c083ddf2fbc4be5718f78f26b5a7616f037b6bbb
dimasio <dlmasio.dlmasio@ya.ru>
2020-03-26 14:09:47 +0300

    add junctions from api to map

--- frontend/js/common.js
+++ frontend/js/common.js
@@ -1,11 +1,12 @@
+//Created by ivasio&dimasio
 //Инициализация карты
-let mapUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+let mapUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       
 let mapLayer = new L.TileLayer(mapUrl, {
     maxZoom: 18
-});
+})
 
-let baseMaps = {"OpenStreetMap": mapLayer};
+let baseMaps = {"OpenStreetMap": mapLayer}
 
 let map = new L.Map('map', {
     center: new L.LatLng(55.7190, 37.5774),
@@ -13,7 +14,7 @@ let map = new L.Map('map', {
     layers: [mapLayer]
 });
 
-//Объект развязки(имя, широта, долгота)
+//Класс - Развязка, конструктор(имя, широта, долгота)
 class Junction {
 	constructor (name, latitude, longitude) {
 		this.name = name,
@@ -21,9 +22,23 @@ class Junction {
 		this.longitude = longitude
 	}
 	addMarker() {
-		return L.marker([this.longitude, this.latitude]).addTo(map).bindPopup(this.name);
+		return L.marker([this.longitude, this.latitude]).addTo(map).bindPopup(this.name)
 	}
 }
 
-let junction = new Junction('МКАД - ш. Энтузиастов', 37.84270, 55.77692);
-junction.addMarker();
+//API адрес развязок
+const apiUrl = 'http://localhost:8888/junctions/'
+
+//Json -> Obj
+async function getJunctions () {
+	const apiResponse = await fetch(apiUrl)
+	const allJunctions = await apiResponse.json()
+
+	//Перебираем объект, инициализируем новый объект класса Junction
+	for (let key in apiJunctions) {
+			let junction = new Junction(apiJunctions[key].name, apiJunctions[key].longitude, apiJunctions[key].latitude)
+			junction.addMarker()
+	}
+}
+//Работаем с backend
+getJunctions()
