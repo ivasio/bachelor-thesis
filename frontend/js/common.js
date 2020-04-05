353f078df0846fbdbca3559d1c0c852e519240e0
dimasio <dlmasio.dlmasio@ya.ru>
2020-04-06 01:23:53 +0300

    new: added junction's methods for routes, marker's color

--- frontend/js/common.js
+++ frontend/js/common.js
@@ -7,9 +7,13 @@
  * ../frontend/index.html 
  * 
  * Source: 
- * git@github.com: 
+ * git@github.com: https://github.com/ivasio/bachelor-thesis.git
  * 
  */
+
+const baseUrl = 'http://localhost:8888/'
+let junctionsUrl = baseUrl + 'junctions/'
+let routesUrl = 'routes_count/'
 
 //Инициализация карты
 let mapUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
@@ -29,59 +33,82 @@ let map = new L.Map('map', {
 
 // Слой с маркер-элементами 
 let markersLayer = new L.LayerGroup() 	 
-  map.addLayer(markersLayer) 
+map.addLayer(markersLayer) 
 	
-  // Инициализация списка макеров
+// Инициализация списка макеров
 let list = new L.Control.ListMarkers({layer: markersLayer})
-  
-  // Меняем цвет маркера по выбранному элементу
-  list.on('item-mouseover', function(e) {
-	e.layer.setIcon(L.icon({
-	  iconUrl: 'img/marker-icon-2x-red.png',
-	  shadowUrl: 'img/marker-shadow.png',
-	}))
-  }).on('item-mouseout', function(e) {
-	e.layer.setIcon(L.icon({
-	  iconUrl: 'img/marker-icon-2x-blue.png',
-	  shadowUrl: 'img/marker-shadow.png',
-	}))
-  })
+map.addControl(list)
 
-  map.addControl(list)
-
-//Класс - Развязка, конструктор(имя, широта, долгота)
 class Junction {
-  constructor (name, latitude, longitude, routes) {
-	this.name = name,
-	this.latitude = latitude,
-	this.longitude = longitude,
-	this.routes = routes
+  constructor (id = Number, name = String, latitude = Number, longitude = Number) {
+    this.id = id,
+    this.name = name,
+    this.latitude = latitude,
+    this.longitude = longitude
   }
-  addMarker() {
-	return L.marker([this.longitude, this.latitude], {junction: this.name}).addTo(markersLayer)
+  // Метод получения количества траекторий развязки
+  getRoutesInfo = async () => {
+    const routesJson = await fetch(junctionsUrl + this.id + '/' + routesUrl)
+    let routesObj = await routesJson.json()
+    return routesObj
+  }
+  // Метод получения коэффициента количества траекторий развязки
+  getRoutesRatio = async (routesObj, maxRoutes) => {
+    let routesRatio = routesObj / maxRoutes
+    return routesRatio
+  }
+  // Метож получения цвета по коэффициенту routesRatio (0-1)
+  getColor = async (routesRatio) => {
+    let color = String
+    if (routesRatio > 0.5) {
+      let R = routesRatio * 255
+      return color = ('rgb(' + R + ', 0, 0)')
+    } else if (routesRatio < 0.5) {
+      let G = routesRatio * 255
+      return color = ('rgb(0,'+ G +', 0)')
+    } else return color = ('rgb(255, 255, 0)')
+  }
+  // Метод добавления маркера на слой markersLayer с цветом iconColor
+  addMarker(iconColor) {
+    return L.marker([this.longitude, this.latitude], {junction: this.name}).setIcon(iconColor).addTo(markersLayer).bindPopup(this.name)
   }
 }
 
-//API адрес развязок
-const apiUrl = 'http://localhost:8888/junctions/'
+routesArr = []
 
-//Json -> Obj
 async function getJunctions () {
-  const apiResponse = await fetch(apiUrl)
-  let apiJunctions = await apiResponse.json()
-  // Сортировка по количеству траекторий
-  apiJunctions.sort(function(a, b) {
-	return Object.keys(a.routes).length - Object.keys(b.routes).length
-  })
-  //Проверяем сортировку
-  console.log(apiJunctions)
-
-  //Перебираем объект, инициализируем новый объект класса Junction
-  for (let key in apiJunctions) {
-	let routes = Object.keys(apiJunctions[key].routes).length
-	let junction = new Junction(apiJunctions[key].name, apiJunctions[key].longitude, apiJunctions[key].latitude, routes)
-	junction.addMarker()
+  const junctionsJson = await fetch(junctionsUrl)
+  let junctionsObj = await junctionsJson.json()
+  // Общее количество развязок
+  let junctionsVal = junctionsObj.length
+  // Собираем массив количества траекторий
+  for (let i=1;i<=junctionsVal;i++) {
+    const routesCountJson = await fetch(junctionsUrl + i + '/' + routesUrl)
+    let routesCountObj = await routesCountJson.json()
+    routesArr.push(routesCountObj)
+  }
+  // Сортируем по убыванию, первый элемент - максимум
+  routesArr.sort(function(a, b) { return b - a; });
+  let routesMax = routesArr[0]
+  // Создаем обьект
+  for (let key in junctionsObj) {
+    let junction = new Junction(junctionsObj[key].id, junctionsObj[key].name, junctionsObj[key].longitude, junctionsObj[key].latitude)
+    // Обработка методов
+    junction.getRoutesInfo().then((routesObj) => {
+      routesRatio = junction.getRoutesRatio(routesObj, routesMax)
+      return routesRatio
+    }).then((routesRatio) => {
+      let junctionsColor = junction.getColor(routesRatio)
+      return junctionsColor
+    }).then((junctionsColor) => {
+      let markerStyle = `background-color: ${junctionsColor}`
+      let iconColor = L.divIcon({
+        className: "map-pin",
+        popupAnchor:  [0, -30],
+        html: `<span style="${markerStyle}" />`
+      })
+      return iconColor
+    }).then((iconColor) => junction.addMarker(iconColor))
   }
 }
-//Работаем с backend
 getJunctions()
