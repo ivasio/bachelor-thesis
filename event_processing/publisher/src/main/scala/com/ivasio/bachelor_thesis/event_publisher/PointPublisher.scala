5a2ecc4be425671fefdb33e640891d76e4e5714f
Ivan Chebotar <van.chebotar@yandex.ru>
2020-06-05 17:11:51 +0300

    new: implemented producing generated points to kafka

--- event_processing/publisher/src/main/scala/com/ivasio/bachelor_thesis/event_publisher/PointPublisher.scala
+++ event_processing/publisher/src/main/scala/com/ivasio/bachelor_thesis/event_publisher/PointPublisher.scala
@@ -1,6 +1,15 @@
 package com.ivasio.bachelor_thesis.event_publisher
 
+import java.time.Instant
+import java.util.concurrent.Future
+import java.util.{Properties, UUID}
+
+import com.ivasio.bachelor_thesis.shared.configuration.SourcedPointKafkaProducerConfig
 import com.ivasio.bachelor_thesis.shared.models.Junction
+import com.ivasio.bachelor_thesis.shared.records.SourcedPoint
+import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord, RecordMetadata}
+import org.apache.kafka.common.serialization.UUIDSerializer
 
 import scala.collection.immutable.Stream
 import scala.math._
@@ -11,14 +20,19 @@ object PointPublisher {
 
   def main(args: Array[String]): Unit = {
     val junction = new Junction(1, "name", 34.0f, 45.0f, 1000)
-    generatePoints(junction).foreach{ case (x, y) =>
-      println(x, y)
-      Thread.sleep(2000)
-    }
+    publishPoints(junction)
+  }
+
+  def publishPoints(junction: Junction): Unit = {
+    val sourceId = UUID.randomUUID
+    val producer = new Producer
+    generateCoordinates(junction)
+      .map{case (x, y) => new SourcedPoint(sourceId, x, y, Instant.now())}
+      .foreach(producer.send)
   }
 
 
-  def generatePoints(junction: Junction) : Stream[(Double, Double)] = {
+  def generateCoordinates(junction: Junction): Stream[(Float, Float)] = {
     val radiusCoordinates = DistanceConverter.toCoordinates(junction.getRadius * 2)
     val loops = 3 + Random.nextInt(5)
 
@@ -27,24 +41,27 @@ object PointPublisher {
     Stream
       .iterate(0.0)(_ + 0.1)
       .map(phi => (phi, ro(phi)))
-      .map{ case (phi, r) => (r * cos(phi), r * sin(phi)) }
+      .map { case (phi, r) => (
+        (junction.getLongitude + r * cos(phi)).asInstanceOf[Float],
+        (junction.getLatitude + r * sin(phi)).asInstanceOf[Float]
+      )}
   }
 
 }
 
 
-//class Producer {
-//  Producer() {
-//    val props = new Properties()
-//    props.put("bootstrap.servers", "localhost:9094")
-//    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-//    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
-//    val producer = new KafkaProducer[String, String](props)
-//    val record = new ProducerRecord[String, String](topic, "key", "value")
-//    producer.send(record)
-//    producer.close()
-//  }
-//}
+class Producer() {
+  val properties: Properties = new SourcedPointKafkaProducerConfig().getProperties
+  properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, UUIDSerializer)
+  val producer: KafkaProducer[UUID, GenericRecord] = new KafkaProducer[UUID, GenericRecord](properties)
+
+  def send(point: SourcedPoint): Future[RecordMetadata] = producer.send(
+    new ProducerRecord[UUID, GenericRecord](
+      properties.getProperty("TOPIC"), point.sourceId, point.toGenericRecord
+    )
+  )
+}
+
 
 object DistanceConverter {
   val met: Double = 500.0
