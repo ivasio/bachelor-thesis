22e1bbf6a320cde7d07a45c8a951507c0b32e84b
Ivan Chebotar <van.chebotar@yandex.ru>
2020-06-07 14:52:25 +0300

    fix: minor fixes in algorithm

--- event_processing/aggregator/src/main/scala/com/ivasio/bachelor_thesis/event_aggregator/RouteProcessor.scala
+++ event_processing/aggregator/src/main/scala/com/ivasio/bachelor_thesis/event_aggregator/RouteProcessor.scala
@@ -1,6 +1,6 @@
 package com.ivasio.bachelor_thesis.event_aggregator
 
-import com.ivasio.bachelor_thesis.shared.configuration.KafkaProducerConfig
+import com.ivasio.bachelor_thesis.shared.configuration._
 import com.ivasio.bachelor_thesis.shared.records._
 import org.apache.avro.generic.GenericRecord
 import org.apache.flink.formats.avro.AvroDeserializationSchema
@@ -8,6 +8,7 @@ import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import org.apache.kafka.clients.consumer.ConsumerConfig
 
 
 object RouteProcessor {
@@ -17,45 +18,49 @@ object RouteProcessor {
     val junctions = setupJunctionsSourceStream
     val points = setupPointsSourceStream
 
-    val filteredPoints = points
+    points
         .assignAscendingTimestamps(_.timestamp.getEpochSecond)
+        .keyBy(_.sourceId)
       .connect(junctions)
-      .keyBy(_.sourceId, _ => 0)
       .flatMap(new RouteFilterFunction)
 
-    filteredPoints
       .keyBy(_._2)
       .window(EventTimeSessionWindows.withGap(Time.minutes(2)))
         .allowedLateness(Time.minutes(3))
         .aggregate(new SquashPointsProcessFunction)
       .addSink(new PointsListJDBCSinkFunction)
+
+    env.execute()
   }
 
+  //noinspection DuplicatedCode
   def setupPointsSourceStream: DataStream[SourcedPoint] = {
     val schemaStore = new SourcedPoint()
     @transient lazy val deserializer = new SourcedPoint()
-    val config = new KafkaProducerConfig()
+    val config = new KafkaConsumerConfig().getProperties
 
     env
       .addSource(
         new FlinkKafkaConsumer[GenericRecord](
           schemaStore.getTopicName,
-          AvroDeserializationSchema.forGeneric(schemaStore.getSchema), config.getProperties
+          AvroDeserializationSchema.forGeneric(schemaStore.getSchema), config
         )
       )
       .map(entry => deserializer.fromGenericRecord(entry))
   }
 
+  //noinspection DuplicatedCode
   def setupJunctionsSourceStream: DataStream[JunctionUpdate] = {
     val schemaStore = new JunctionUpdate()
     @transient lazy val deserializer = new JunctionUpdate()
-    val config = new KafkaProducerConfig()
+    val config = new KafkaProducerConfig().getProperties
+    config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
 
     env
       .addSource(
         new FlinkKafkaConsumer[GenericRecord](
           schemaStore.getTopicName,
-          AvroDeserializationSchema.forGeneric(schemaStore.getSchema), config.getProperties
+          AvroDeserializationSchema.forGeneric(schemaStore.getSchema), config
         )
       )
       .map(entry => deserializer.fromGenericRecord(entry))
